1ea936a81b08cacfb3554c624b5ae9acb233004b
Noam shushan <noam8shu@gmail.com>
2022-02-20 17:53:17 +0200

    Add navgations. need more desing

--- AstronomyProject/Gui/ViewModels/MainWindowViewModel.cs
+++ AstronomyProject/Gui/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,21 @@ namespace Gui.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        
+        readonly IRegionManager _regionManager;
+
+        public DelegateCommand<string> NavigateCommand { get; private set; }
+
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+
+            NavigateCommand = new DelegateCommand<string>(Navigate);
+        }
+
+        private void Navigate(string navigatePath)
+        {
+            if (!string.IsNullOrEmpty(navigatePath))
+                _regionManager.RequestNavigate("ContentRegion", navigatePath);
+        }
     }
 }
